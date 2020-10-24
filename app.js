c1399f2abb67b91c319d597a7868464eae872eee
sergio <s.brianski@yahoo.com>
2020-10-24 22:05:41 +0300

    Vk test #2

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.get("/", function (req, res) {
   res.render("main", {});
 });
 
+app.get("/dev/Login", function (req, res) {
+  res.send("uid: " + req.param('uid') + "<br>first name: " + req.param('first_name') + "<br>second name: " +
+      req.param('second_name') + "<br>photo url: " + req.param('photo'));
+});
+
 app.post("/registration", function (req, res){
   res.render("registration", {})
 });
