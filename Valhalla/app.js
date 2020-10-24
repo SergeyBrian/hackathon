5e84a49fa97f1140c72ff805b06bb534fb3df4d1
Sergio Brianski <s.brianski@yahoo.com>
2020-10-24 19:30:54 +0300

    App

--- Valhalla/app.js
+++ Valhalla/app.js
@@ -15,6 +15,10 @@ app.get("/", function (req, res) {
   res.render("main", {});
 });
 
+app.post("/registration", function (req, res){
+  res.render("registration", {})
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000.");
 });
